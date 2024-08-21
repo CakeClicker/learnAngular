1afd3ed21ab4644552ef359e112dc0e1100f2f6a
langkluk <lukas.langklotz@de.sauter-bc.com>
2024-08-21 16:58:14 +0200

    updated some things

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+likeCounter = 0;
+constructor(private snackbar: MatSnackBar){}
+
+likeShibe(event: any) {
+  this.likeCounter++;
+   this.snackbar.open("Thx for liking", "dismiss",
+    {duration:3000}
+  )
+}
+shareShibe(event: any) {
+  this.snackbar.open("Thx for sharing", "dismiss",
+    {duration:3000}
+  )
+}
 
 }
